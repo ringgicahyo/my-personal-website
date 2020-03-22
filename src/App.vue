d20c8fddf5c329b97458768e6738e3772da899bb
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2020-03-22 16:54:14 +0700

    edit landing page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img id="logo" src="./assets/r_logo_red.png">
     <router-view/>
   </div>
 </template>
@@ -13,11 +13,16 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#logo{
+  max-width: 30%;
+  height: auto;
 }
 </style>
